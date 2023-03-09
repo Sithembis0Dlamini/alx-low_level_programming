8df62c8c244d3b81c9df5e3bf81b2dfe7c48b03c
Sithembis0Dlamini <mrsithembisodlamini@gmail.com>
2023-03-09 11:13:32 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,21 +8,21 @@
  */
 int helperFunction(int num, int i)
 {
-	if (i < num)
-        {
-	if (num % i == 0)
-        { 
-	return (0);
-       	}
-        else
-        {
-        return (helperFunction(num, i + 1));
-        }
-        }
-        else
-        {
-	return (1);
-        }
+if (i < num)
+{
+if (num % i == 0)
+{ 
+return (0);
+}
+else
+{
+return (helperFunction(num, i + 1));
+}
+}
+else
+{
+return (1);
+}
 }
 /**
  * is _prime_number - checks if number is prime or not.
@@ -34,12 +34,12 @@ int helperFunction(int num, int i)
 
 int is_prime_number(int n)
 {
-        if (n <= 1)
-        {
- 	return (0);
-        }
-        else
-        {
-	return (helperFunction(n, 2));
-        }
+if (n <= 1)
+{
+return (0);
+}
+else
+{
+return (helperFunction(n, 2));
+}
 }
