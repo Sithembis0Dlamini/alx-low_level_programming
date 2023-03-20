e283598ec52a6539f12bba3fa22548d8fc4ca2e9
Sithembis0Dlamini <mrsithembisodlamini@gmail.com>
2023-03-20 08:56:08 -0700

    dog.h

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include "dog.h"
 
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
